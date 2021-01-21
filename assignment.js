c55af35da591d85a6a916be3377f39677048b4a5
Shahed70 <58668503+Shahed70@users.noreply.github.com>
2021-01-22 00:57:37 +0600

    three function has been completed

---
+++ assignment.js
@@ -0,0 +1,52 @@
+
+
+// Kilometer to meter function
+
+function kilometerToMeter(kilometer){
+      
+        var meter = kilometer * 1000;
+        return meter;
+}   
+
+
+//Budget calculator function
+
+function budgetCalculator(numberOfWatch, numberOfPhone, numberOfLaptop){
+
+    var totalWatchPrice = numberOfWatch * 50;
+    
+    var totalPhonePrice = numberOfPhone * 100;
+
+    var totalLaptopPrice = numberOfLaptop * 500;
+
+    var totalCost = totalWatchPrice + totalPhonePrice + totalLaptopPrice;
+
+    return totalCost;
+
+}
+
+
+// Hotel cost function
+
+function hotelCost(numberOfDays){
+    
+    var totalHotelCost = 0;
+    
+    if(numberOfDays <=10){
+        totalHotelCost = 10 * 100
+    }
+    else if(numberOfDays <= 20){
+       var firstTimeHotelCost = 10 * 100;
+       var remainingDays = numberOfDays - 10;
+       var secondTimeHotelCost = remainingDays * 80;
+       totalHotelCost = firstTimeHotelCost + secondTimeHotelCost;
+    }
+    else{
+        var firstTimeHotelCost = 10 * 100;
+        var secondTimeHotelCost = 10 * 80;
+        var remainingDays = numberOfDays - 20;
+        var lastDaysHotelCost = remainingDays * 50;
+        totalHotelCost = firstTimeHotelCost + secondTimeHotelCost + lastDaysHotelCost;
+    }
+    return totalHotelCost;
+}
